07b03eb2e3d198bea3da26a943a6f5e5ca68094d
gonzaloandres12 <114397689+gonzaloandres12@users.noreply.github.com>
2022-10-15 21:30:57 +0200

    Add files via upload

--- SubirFicheros/subirFicheros.php
+++ SubirFicheros/subirFicheros.php
@@ -6,6 +6,16 @@ function numeroFicheros(){
     return $numeroFicehros;
 }
 
+function sizeTodosArchivos(){
+    $tamanioFicheroT =0;
+    for ($i=0; $i <numeroFicheros(); $i++) {  
+        $tamanioFicheroT  += $_FILES['archivos']['size'][$i];
+    }
+
+    return $tamanioFicheroT;
+}
+
+
 if (count($_POST) == 0 ){
         echo "Error: se supera el tamaño máximo de un petición POST ";
     }
@@ -15,21 +25,51 @@ else if ((!isset($_FILES['archivos']['name']))) {
 }
 else{
      // se reciben el directorio de alojamiento y el archivo
+     //diredtorio en linux
      $directorioSubida = "home/alumnoa/tallerphp/SubirFicheros/imgusers";
-     $directorioSubidaWin = "";
-    echo $directorioSubida."<br>";
-    if (is_dir($directorioSubida) && is_writable($directorioSubida)) {
-        echo "Dir correcto y se puede escribir";
-        for ($i=0; $i <numeroFicheros(); $i++) { 
+     //directorio en windos
+     $directorioSubidaWin = "C:\Users\gonza\OneDrive\Escritorio\imguser";
+    echo $directorioSubidaWin."<br>";
+    if (is_dir($directorioSubidaWin) && is_writable($directorioSubidaWin)) {
+        echo "Dir correcto y se puede escribir<br>";
+        echo "Numero de ficheros=>".numeroFicheros()."<br>";
+        echo "Tamaño total de ficheros=>".sizeTodosArchivos()."<br>";
+    
+        if(sizeTodosArchivos()<300000){        
+            for ($i=0; $i <numeroFicheros(); $i++) { 
             $nombreFichero   = $_FILES['archivos']['name'][$i];
             $tipoFichero     = $_FILES['archivos']['type'][$i];
             $tamanioFichero  = $_FILES['archivos']['size'][$i];
             $temporalFichero = $_FILES['archivos']['tmp_name'][$i];
             $errorFichero    = $_FILES['archivos']['error'][$i];
-            if (move_uploaded_file($temporalFichero,  $directorioSubida .'/'. $nombreFichero) == true) {
-            echo 'Archivo guardado en: ' . $directorioSubida .'/'. $nombreFichero . ' <br/>';
-            }       
+                if($tipoFichero == "image/png" or $tipoFichero == "image/jpeg"){
+                    if($tamanioFichero<=200000){
+                        if(!file_exists($directorioSubidaWin.'/'.$nombreFichero)){
+                            if (move_uploaded_file($temporalFichero,  $directorioSubidaWin .'/'. $nombreFichero) == true)
+                            {
+                                echo 'Archivo guardado en:=>' . $directorioSubidaWin .'/'. $nombreFichero . ' <br/>';
+                            }else
+                            {
+                                echo  'Error al guardar el archivo=>'.$nombreFichero.' <br/>';
+                            }
+                        }else
+                        {
+                            echo "Error El fichero ya existe=>".$nombreFichero.' <br/>';
+                        }
+                    }else
+                    {
+                        echo "Error El tamaño del fichero es demasio grande=>".$nombreFichero.' <br/>';
+                    }
+                }else
+                {
+                    echo "Error El tipo de fichero no es valido o es demasiado grande=>".$nombreFichero.' <br/>';
+                }          
+            }
+        }else
+        {
+            echo "Error El tamaño total de los archivos supera el valido";
         }
+
     }else{
         echo "El directorio no es correcto o no tiene permisos de escritura ";
     }
